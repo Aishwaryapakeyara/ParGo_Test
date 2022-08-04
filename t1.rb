5fbf163777100bcaf2340f88f8a1515c6f10d56c
Aishwaryapakeyara <apakeyara@gmail.com>
2022-08-04 20:04:27 +0530

    Updated files

--- t1.rb
+++ t1.rb
@@ -1,7 +1,11 @@
-print('Enter K: ')
-num2 = gets.chomp!.to_i
-print('Enter N: ')
-num1 = gets.chomp!.to_i
+# frozen_string_literal: true
+
+def check_int(num)
+  Integer(num)
+rescue ArgumentError
+  p 'Please enter an integer.'
+  exit
+end
 
 def test_input(num1, num2)
   num_array = num1.to_s.chars.map(&:to_i)
@@ -13,4 +17,21 @@ def test_input(num1, num2)
   num_array.inject { |n, d| n * 10 + d }
 end
 
-puts test_input(num1, num2)
+def run_testtt(number1, number2)
+  if number1.between?(100, 999) == false
+    p 'Please enter value of N between 100 and 999'
+  elsif number2.between?(0, 30) == false
+    p 'Please enter value of K between 0 and 30'
+  else
+    puts test_input(number1, number2)
+  end
+end
+
+print('Enter N: ')
+num1 = gets.chomp
+num1 = check_int(num1)
+print('Enter K: ')
+num2 = gets.chomp
+num2 = check_int(num2)
+
+run_testtt(num1, num2)
--- t1_test.rb
+++ t1_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'test/unit'
 require_relative './t1'
 
@@ -47,13 +49,35 @@ class T1Test < Test::Unit::TestCase
     assert_equal 999, test_input(999, 8)
   end
 
-  def test_pair11
+  def test_pair12
     assert_equal 999, test_input(999, 9)
   end
 
-  # def test_num1_not_given
-  #   assert_equal 999, test_input(999, 9)
+  def test_num1_negative
+    assert_equal 'Please enter value of N between 100 and 999', run_testtt(-543, 9)
+  end
+
+  def test_num1_below_the_range
+    assert_equal 'Please enter value of N between 100 and 999', run_testtt(43, 9)
+  end
+
+  def test_num1_above_the_range
+    assert_equal 'Please enter value of N between 100 and 999', run_testtt(4143, 9)
+  end
+
+  def test_num1_not_given
+    assert_equal 'Please enter an integer.', check_int('')
+  end
+  # def test_num2_out_of_range
+  #   assert_equal 'Please enter value of k between 0 and 30', test_input(43, 78)
   # end
 
-  #flunk 'Not done testing yet.'
+  # def test_num2_not_given
+  #   assert_raise ArgumentError do
+
+  #   end
+  # end
+
+  # def test_num1_not_given
+  #   assert_not_nil('Please enter an integer between 100 and 999', )
 end
